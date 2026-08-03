136bc7aa51d51017640a14618b2269e1f8161ee3
t3ch <w4d4f4k@gmail.com>
2026-08-03 00:47:02 +0000

    refactor: split Log.echo into _echo_opts, _echo_accumulate, _echo_print, _echo_finish

--- src/Log.py
+++ src/Log.py
@@ -48,72 +48,87 @@ class Log:
 	
 	#
 	def echo(self,text,opts={}):
-		#print("Log.echo() START DEBUG opts",opts)
-		wait                = True
-		opt_end             = opts['end'] if 'end' in opts else '\r\n'
-		opt_flush           = opts['flush'] if 'flush' in opts else False
-		opt_color           = opts['color'] if 'color' in opts else False # True or False
-		opt_colorValue      = opts['colorValue'] if 'colorValue' in opts else None # True or False
-		opt_debugOnly       = opts['debugOnly'] if 'debugOnly' in opts else True
-		opt_echoByNewLine   = opts['echoByNewLine'] if 'echoByNewLine' in opts else False
-		opt_echoByLength    = opts['echoByLength'] if 'echoByLength' in opts else False
-		opt_streamDone      = opts['streamDone'] if 'streamDone' in opts else False
-		opt_returnStream    = opts['returnStream'] if 'returnStream' in opts else False
-		opt_speak           = opts['speak'] if 'speak' in opts else False
-		#
-		_is_streaming = opt_echoByNewLine or (opt_echoByLength != False and opt_echoByLength > 0)
-		if opt_debugOnly and self.debug==False:
+		o = self._echo_opts(opts)
+		if o['debugOnly'] and self.debug==False:
 			#print("Log.echo() DEBUG D2, streamData.len: {}".format( len(self.streamData) ))
 			return False
-		#--
-		#
-		if opt_streamDone:
-			self.streamData = self.streamData+text
-			wait = False
-		elif opt_echoByNewLine:
-			self.streamData = self.streamData+text
-			if rmatch(self.streamData,"\n") or rmatch(self.streamData,".*\n") or rmatch(self.streamData,".*\n.*"):
-				wait = False
-		elif opt_echoByLength!=False and opt_echoByLength>0:
-			self.streamData = self.streamData+text
-			if len(self.streamData)>=opt_echoByLength:
-				wait = False
-		else:
-			self.streamData = self.streamData+text
-			wait = False
-		#--
-		#
+		wait = self._echo_accumulate(text, o)
 		if wait:
 			#print("Log.echo() DEBUG D1, streamData.len: {}".format( len(self.streamData) ))
 			return False
-		#--
-		prefix=""
-		if self.debug or opt_debugOnly:
+		prefix = ""
+		if self.debug or o['debugOnly']:
 			prefix = "DEBUG "
-		#
-		if opt_color:
+		self._echo_print(prefix, o)
+		return self._echo_finish(o, wait,
+			o['echoByNewLine'] or (o['echoByLength']!=False and o['echoByLength']>0))
+
+	#
+
+	def _echo_opts(self, opts):
+		return {
+			'end'           : opts['end'] if 'end' in opts else '\r\n',
+			'flush'         : opts['flush'] if 'flush' in opts else False,
+			'color'         : opts['color'] if 'color' in opts else False,
+			'colorValue'    : opts['colorValue'] if 'colorValue' in opts else None,
+			'debugOnly'     : opts['debugOnly'] if 'debugOnly' in opts else True,
+			'echoByNewLine' : opts['echoByNewLine'] if 'echoByNewLine' in opts else False,
+			'echoByLength'  : opts['echoByLength'] if 'echoByLength' in opts else False,
+			'streamDone'    : opts['streamDone'] if 'streamDone' in opts else False,
+			'returnStream'  : opts['returnStream'] if 'returnStream' in opts else False,
+			'speak'         : opts['speak'] if 'speak' in opts else False,
+		}
+
+	#
+
+	def _echo_accumulate(self, text, o):
+		"""Accumulate text into the stream buffer. Returns True when we
+		should keep waiting (streaming), False when ready to emit."""
+		if o['streamDone']:
+			self.streamData = self.streamData+text
+			return False
+		elif o['echoByNewLine']:
+			self.streamData = self.streamData+text
+			if rmatch(self.streamData,"\n") or rmatch(self.streamData,".*\n") or rmatch(self.streamData,".*\n.*"):
+				return False
+		elif o['echoByLength']!=False and o['echoByLength']>0:
+			self.streamData = self.streamData+text
+			if len(self.streamData)>=o['echoByLength']:
+				return False
+		else:
+			self.streamData = self.streamData+text
+			return False
+		return True
+
+	#
+
+	def _echo_print(self, prefix, o):
+		if o['color']:
 			#
 			tmpcolor = self.CRED
-			if opt_colorValue!=None:
-				if opt_colorValue=='green':
+			if o['colorValue']!=None:
+				if o['colorValue']=='green':
 					tmpcolor = self.CGREEN
-				if opt_colorValue=='orange':
+				if o['colorValue']=='orange':
 					tmpcolor = self.CORANGE
-			if opt_returnStream==False:
-				if self.hSpeak!=None and opt_speak:
+			if o['returnStream']==False:
+				if self.hSpeak!=None and o['speak']:
 					#print("Log().echo() d1 len {}".format( len(self.streamData) ))
 					self.hSpeak.Parse( self.streamData )
-				print("{}{}{}{}".format( prefix, tmpcolor, self.streamData, self.CNC ),end=opt_end, flush=opt_flush)
+				print("{}{}{}{}".format( prefix, tmpcolor, self.streamData, self.CNC ),end=o['end'], flush=o['flush'])
 		else:
-			if opt_returnStream==False:
+			if o['returnStream']==False:
 				if self.handle.opt_response_with != None:
 					print("Log.echo() opt_response_with() START d3")
 					self.handle.opt_response_with( self.streamData )
 				else:
 					#print("Log().echo() d2 len {}".format( len(self.streamData) ))
-					print("{}{}".format( prefix, self.streamData ),end=opt_end, flush=opt_flush)
-		#
-		if opt_streamDone or wait==False:
+					print("{}{}".format( prefix, self.streamData ),end=o['end'], flush=o['flush'])
+
+	#
+
+	def _echo_finish(self, o, wait, is_streaming):
+		if o['streamDone'] or wait==False:
 			#
 			if self.handle.opt_response_with != None:
 				print("Log.echo() opt_response_with() START d1")
@@ -123,15 +138,15 @@ class Log:
 				self.handle.opt_response_done( self.streamData )
 			#
 			tmp=""
-			if opt_returnStream:
+			if o['returnStream']:
 				tmp = self.streamData
 				self.streamData = ""
 				return tmp
-			if not _is_streaming:
+			if not is_streaming:
 				self._log_background(self.streamData)
 			self.streamData = ""
 		return False
-	
+
 	#
 	def Test(self):
 		print("Log.Test() START!")
